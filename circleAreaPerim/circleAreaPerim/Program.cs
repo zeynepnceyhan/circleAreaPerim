9e3b6c31756265eb51a2350e91e8842aff1876eb
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-02 12:11:24 +0300

    area and perimeter of a circle
    internship

---
+++ circleAreaPerim/circleAreaPerim/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = 3.14;
+            double perimeter, area;
+            int radius;
+            Console.WriteLine("enter a radius value: ");
+            radius = Convert.ToInt32(Console.ReadLine());
+
+
+            perimeter = 2 * pi * radius;
+            area = pi * radius * radius;
+
+            Console.WriteLine("The perimeter of the circle: " + perimeter);
+            Console.WriteLine("The area of the circle: " + area);
+            Console.ReadLine();
+        }
+
+    }
+
+}
